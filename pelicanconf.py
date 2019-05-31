5bd062d403ff78316d13a8a0f8edadb22b815f60
NaotoYamaguchi <utyamaguchi59@gmail.com>
2019-06-01 08:47:17 +0900

    add: render_math

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,3 +35,9 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = ['render_math']
+
+#PLUGIN_PATH = ['../pelican-plugins']
+#PLUGINS = ['render_math']
